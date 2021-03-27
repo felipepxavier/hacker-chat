23087f04ea68624321f5bc42f4b3eb2b611cca7f
Felipe Pedroso Xavier <felipe.sadler@gmail.com>
2021-03-27 11:18:57 -0300

    feat: add cliConfig

--- client/index.js
+++ client/index.js
@@ -1,5 +1,19 @@
+/*
+ node index.js \
+ --username felipepxavier \
+ --room sala01 \
+ --hostUri localhost
+ */
+
+
 import Events from 'events';
+import CliConfig from './src/cliConfig.js';
 import TerminalController from "./src/terminalController.js";
+
+const [nodePath, filePath, ...commands] = process.argv
+
+const config = CliConfig.parseArguments(commands)
+console.log('config', config)
 
 const componentEmitter = new Events()
 
