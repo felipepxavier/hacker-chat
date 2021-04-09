2f015bbd5b95a1d94a70413e0386fc9bd8f60b45
Felipe Pedroso Xavier <felipe.sadler@gmail.com>
2021-04-08 23:06:47 -0300

    feat: create eventManager client

--- client/src/eventManager.js
+++ client/src/eventManager.js
@@ -1,3 +1,5 @@
+import { constants } from "./constants.js"
+
 export default class EventManager {
     constructor({ componentEmitter, socketClient }) {
         this.componentEmitter = componentEmitter 
@@ -5,6 +7,10 @@ export default class EventManager {
     }
 
     joinRoomAndAwaitForMessages(data){
+        this.socketClient.sendMessage(constants.events.socket.JOIN_ROOM, data)
 
+        this.componentEmitter.on(constants.events.app.MESSAGE_SENT, msg =>{
+            this.socketClient.sendMessage(constants.events.socket.MESSAGE, msg)
+        })
     }
 }
